40545c28ef20b4d1557483669c3918f0f746c2e1
Supattralhing <supattralhing@gmail.com>
2026-09-10 15:53:04 +0700

    Get and Post

--- server/main.go
+++ server/main.go
@@ -1,7 +1,24 @@
 package main
 
-import "fmt"
+import "github.com/gin-gonic/gin"
 
 func main() {
-	fmt.Print("hello")
+  router := gin.Default()
+
+  //Get
+  router.GET("/student", func(c *gin.Context) {
+    c.JSON(200, gin.H{
+      "message": "get student complete!",
+    })
+  })
+
+  //Post
+  router.POST("/student", func(c *gin.Context) {
+    c.JSON(200, gin.H{
+      "message": "create student complete!",
+    })
+  })
+
+  router.Run() // listens on 0.0.0.0:8080 by default
+
 }
